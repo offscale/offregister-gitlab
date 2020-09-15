e420dac8e8c050d56910ef31adac2968489f03f8
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offregister_gitlab/ubuntu/__init__.py
+++ offregister_gitlab/ubuntu/__init__.py
@@ -5,10 +5,14 @@ from offregister_fab_utils.apt import apt_depends
 
 
 def install0(SERVER_NAME, *args, **kwargs):
-    apt_depends('curl', 'openssh-server', 'ca-certificates')
-    apt_depends('postfix')
+    apt_depends("curl", "openssh-server", "ca-certificates")
+    apt_depends("postfix")
 
-    external_url = '{protocol}://{server_name}'.format(protocol=kwargs.get('https') or 'http', server_name=SERVER_NAME)
-    sudo('curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | bash')
+    external_url = "{protocol}://{server_name}".format(
+        protocol=kwargs.get("https") or "http", server_name=SERVER_NAME
+    )
+    sudo(
+        "curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | bash"
+    )
     with shell_env(EXTERNAL_URL=external_url):
-        apt_depends('gitlab-ee')
+        apt_depends("gitlab-ee")
