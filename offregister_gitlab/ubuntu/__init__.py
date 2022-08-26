059089ef698359c707c50a148463ab675efe878a
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-08-25 22:06:30 -0400

    [*.py] Update to threadsafe Python 3 compatible Fabric ; [README.md] Update support Python version range ; [LICENSE-{APACHE,MIT}] Update copyright year range

--- offregister_gitlab/ubuntu/__init__.py
+++ offregister_gitlab/ubuntu/__init__.py
@@ -1,17 +1,16 @@
 from fabric.context_managers import shell_env
-from fabric.operations import sudo
 from offregister_fab_utils.apt import apt_depends
 
 
 def install0(SERVER_NAME, *args, **kwargs):
-    apt_depends("curl", "openssh-server", "ca-certificates")
-    apt_depends("postfix")
+    apt_depends(c, "curl", "openssh-server", "ca-certificates")
+    apt_depends(c, "postfix")
 
     external_url = "{protocol}://{server_name}".format(
         protocol=kwargs.get("https") or "http", server_name=SERVER_NAME
     )
-    sudo(
+    c.sudo(
         "curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | bash"
     )
     with shell_env(EXTERNAL_URL=external_url):
-        apt_depends("gitlab-ee")
+        apt_depends(c, "gitlab-ee")
